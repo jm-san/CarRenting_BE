b8ae0909e6f8ec26279d9bc85ece913a710f075a
Manu <sanchezguemes.manu@gmail.com>
2026-07-09 00:24:45 +0200

    Solucionada incongruencia de ApiResponse con su interfaz.

--- Application/Common/Models/IApiResponse.cs
+++ Application/Common/Models/IApiResponse.cs
@@ -2,7 +2,7 @@ namespace Application.Common.Models
 {
     public interface IApiResponse
     {
-        string ApiResponseMessage { get; set; }
-        string Message { get; set; }
+        string? ApiResponseMessage { get; set; }
+        string? Message { get; set; }
     }
 }
